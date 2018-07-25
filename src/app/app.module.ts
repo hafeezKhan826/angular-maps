cd7963156bb3541e53d2380be7563cd8aa7b7c06
Sonam <sonam@zool.in>
2018-07-25 18:42:31 +0530

    Revert "Initial commit for a angular google maps"
    This reverts commit afdf94d5d2fff7e3821aa603569c3fd97b7a1c4a.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,18 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-import { AgmCoreModule, GoogleMapsAPIWrapper } from "@agm/core";
-import { AgmSnazzyInfoWindowModule } from "@agm/snazzy-info-window";
-import { AppComponent } from "./app.component";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    AgmCoreModule.forRoot({
-      apiKey: "AIzaSyDK4NvjsQQr6q_KCybHtHH1yuy7G4furL0"
-    }),
-    AgmSnazzyInfoWindowModule
+  declarations: [
+    AppComponent
   ],
-  providers: [GoogleMapsAPIWrapper],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
